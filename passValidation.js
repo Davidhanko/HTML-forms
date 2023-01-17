4ea110d6960dc66dbf24d59377832c37f9dbf886
Davihan11 <davihan11@gmail.com>
2023-01-17 21:01:24 +0100

    changed check to bool

--- passValidation.js
+++ passValidation.js
@@ -1,17 +1,17 @@
 let passedValidation = false;
-let check =0;
+let check = true;
 function passValidation() {
     let pass1 = document.getElementById("password").value;
     let pass2 = document.getElementById("confirmPassword").value;
     if (pass1 !== pass2) {
-        if(check === 0) {
+        if(check) {
         let p = document.createElement("p");
         p.innerHTML = `<sup>*</sup>Passwords do not match`;
         document.querySelector(".error").appendChild(p);
         }
-        passedValidation=false;
+        passedValidation= false;
         passVad(passedValidation);
-        check=check+1;
+        check=false;
     }
     passedValidation=true;
     passVad(passedValidation);
